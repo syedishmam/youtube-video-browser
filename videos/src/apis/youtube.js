f6831b765d2ef5b4ce806e5b14c1119fd3ec9a0d
Ish <syedishmam17@gmail.com>
2019-08-16 08:20:30 -0400

    Set YouTube api search boilerplate

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyDJS7mIeJc-kICQhM-umf567xT1gE1vJX8';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
