851f13aa6d21ca40f174f2ac49001b985a17b5d3
Ish <syedishmam17@gmail.com>
2019-08-16 08:34:33 -0400

    Display number of videos found after api request

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -3,19 +3,23 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-    onTermSubmit = (term) => {
+    state = { videos: [] };
 
-        youtube.get('/search', {
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
+
+        this.setState({ videos: response.data.items });
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onTermSubmit={this.onTermSubmit} />
+                I have {this.state.videos.length} videos.
             </div>
         )
     }
--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
     onFormSubmit = (e) => {
         e.preventDefault();
 
-        this.props.onFormSubmit(this.state.term);
+        this.props.onTermSubmit(this.state.term);
     }
 
     render() {
