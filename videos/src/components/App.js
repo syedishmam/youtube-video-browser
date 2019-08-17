22580a0541e124d08f7b7f3d48a66e24d33fc8ce
Ish <syedishmam17@gmail.com>
2019-08-17 16:59:42 -0400

    Display default video on player

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -7,8 +7,13 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
 
+    componentDidMount() {
+        
+    }
+
     //When a search term is submitted by user, fetch YouTube API with search term
     //Then set video state to be the API response
+    //Set default video played to be first video on list
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
             params: {
@@ -16,7 +21,10 @@ class App extends React.Component {
             }
         });
 
-        this.setState({ videos: response.data.items });
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     };
 
     //When a video is clicked on change state of selectedVideo
