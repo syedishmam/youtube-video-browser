fc73adfbe8b1f742d233e7fa8d72c985e8531c4a
Ish <syedishmam17@gmail.com>
2019-08-17 16:40:58 -0400

    Add video description to VideoItem

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -6,7 +6,12 @@ const VideoDetail = ({ video }) => {
     }
 
     return (
-        <div>{video.snippet.title}</div>
+        <div>
+            <div className="ui segment">
+                <h4 className="ui header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
+        </div>
     )
 }
 
