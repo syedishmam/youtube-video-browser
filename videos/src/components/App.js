9b7b55a94d1d6d38b317b6648031e7153f5e7a84
Ish <syedishmam17@gmail.com>
2019-08-17 15:46:56 -0400

    Resize video list

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,7 +4,7 @@ import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
@@ -15,6 +15,10 @@ class App extends React.Component {
 
         this.setState({ videos: response.data.items });
     };
+
+    onVideoSelect = () => {
+        
+    }
 
     render() {
         return (
